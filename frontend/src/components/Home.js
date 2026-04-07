e4248cdfbe9740f868288f7441fbc1f90ed936a2
Parminder Singh <parmindersingh28999@gmail.com>
2026-04-07 13:58:49 -0400

    Home

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,31 +5,31 @@ const categories = [
   {
     name: 'Fresh Produce',
     filterCategory: 'Vegetables',
-    image: 'https://i.ibb.co/Y4NMx4hF/produce.png',
+    image: '',
     color: '#dcfce7'
   },
   {
     name: 'Dairy',
     filterCategory: 'Dairy',
-    image: 'https://i.ibb.co/d4ZMKMvn/dairy.png',
+    image: '',
     color: '#fef9c3'
   },
   {
     name: 'Punjabi Specials',
     filterCategory: 'Pantry',
-    image: 'https://i.ibb.co/GvK1ZMSX/punjabi.png',
+    image: '',
     color: '#fef3c7'
   },
   {
     name: 'Snacks',
     filterCategory: 'Snacks',
-    image: 'https://i.ibb.co/5g5DThBf/snacks.png',
+    image: '',
     color: '#fed7aa'
   },
   {
     name: 'Household',
     filterCategory: 'Household',
-    image: 'https://i.ibb.co/nNH5hcDj/household.png',
+    image: '',
     color: '#e0f2fe'
   },
 ];
